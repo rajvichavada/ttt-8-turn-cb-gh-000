04e1dabdbab30eeeee702fb2de8c2d8fb2743b30
Rajvi Chavada <rajvic@my.yorku.ca>
2018-05-21 00:29:54 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+    puts "-----------"
+  puts "   |   |   "
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+if position_taken?(board, index)
+  return false
+elsif index.between?(0,8)
+  return true
+else
+end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# code your #position_taken? method here!
+def position_taken?(board, index)
+if board[index] == ""
+  false
+elsif board[index] == " "
+  false
+elsif board[index] == nil
+  false
+elsif board[index] == 'X' || board[index] == 'O'
+  true
+end
+end
